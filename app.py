1006935ec304007d49da7f3e5c13c878a996f389
losterkol529@wp.pl <losterkol529@wp.pl>
2021-04-21 00:43:09 +0200

    zjazd 4 zadania

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
+USERS = [
+    ("uzytkownik1",1),
+    ("uzytkownik2",2),
+    ("uzytkownik3",3),
+]
 
 
-@app.route('/zad1', methods=('GET', 'POST'))
-def zad1():
-    return 'You said:{}'.format(request.form['freetext'])
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
 
 
-@app.route('/zad2', methods=('GET', 'POST'))
-def zad2():
-    a = float(request.form['firstnumber'])
-    b = float(request.form['secondnumber'])
-    char = (request.form['znaki'])
+@app.route('/calculator/<string:char>/<int:a>/<int:b>', methods=('GET', 'POST'))
+def calculator(char,a,b):
     result = 0
 
     if char == 'dodawanie':
@@ -29,20 +31,13 @@ def zad2():
     return str(result)
 
 
-@app.route('/zad3', methods=('GET', 'POST'))
-def zad3():
-    correctpass = "abc"
-    writtenlog = request.form['login']
-    writtenpass = request.form['pass']
-    if correctpass == writtenpass:
-        return render_template('index2.html')
+@app.route("/user/<int:user_id>")
+def user(user_id):
+    x = [x for x in USERS if x[1]==user_id]
+    if x:
+        return str(x[0])
     else:
-        return render_template('index.html', value=writtenlog, value1='Incorrect Password')
-
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+        return "Brak uzytkownika w bazie!"
 
 
 if __name__ == '__main__':
